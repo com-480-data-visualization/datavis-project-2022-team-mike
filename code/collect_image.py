46a003284de4d443b4260de1ad18c225a7b1ef27
Rygone <44498456+Rygone@users.noreply.github.com>
2022-06-03 02:58:57 +0200

    up

--- code/collect_image.py
+++ code/collect_image.py
@@ -1,5 +1,7 @@
 import requests
+from time import time
 from util import save_dataframes, load_dataframes
+
 
 def get_image(row):
     def getafter(text, substr):
@@ -9,10 +11,11 @@ def get_image(row):
         return text[:text.find(substr)]
 
     def test_is_valid_image(img, extension, row):
-        res = getbefore(img, '.' + extension).split('/')[-1].replace('_', '').replace('-', '').replace(' ', '')
+        res = getbefore(
+            img, '.' + extension).split('/')[-1].replace('_', '').replace('-', '').replace(' ', '')
         for t in row['Name'].split(' '):
             if(t.replace('.', '') in res):
-                res = res.replace(t,'')
+                res = res.replace(t, '')
             else:
                 return False
         return len(res) == 0
@@ -23,7 +26,8 @@ def get_image(row):
         if(t not in res):
             res.append(t)
     text = '+'.join(res)
-    r = requests.get(f"http://logos.fandom.com/wiki/Special:Search?scope=internal&query={text}&ns%5B0%5D=6&filter=imageOnly")
+    r = requests.get(
+        f"http://logos.fandom.com/wiki/Special:Search?scope=internal&query={text}&ns%5B0%5D=6&filter=imageOnly")
     text = r.text
     first = None
     while ((len(text) > 0) and ('"unified-search__result__header"' in text)):
@@ -33,19 +37,41 @@ def get_image(row):
         text = getafter(text, '<img')
         text = getafter(text, 'src="')
         res = getbefore(text, '"')
-        if(first is None):
-            first = res
         for extension in ['svg', 'jpg', 'png', 'webp']:
             if(f'.{extension}' in res):
                 res = getbefore(res, extension) + extension
+                if(first is None):
+                    first = res
                 if test_is_valid_image(res, extension, row):
                     return res
     return first
 
-def add_image(df):
-    val = df.apply(lambda row: (f"{row['Name']} {row['Platform']} {row['Publisher']}", get_image(row)), axis=1)
-    val.apply(lambda x : print(x))
 
-df = load_dataframes("vgsales_cleand")
-add_image(df.head(10))
-#save_dataframes(df, "vgsales")
+def add_image(path):
+    global i, d, t, df
+    df = load_dataframes(path)
+    i = 0
+    d = 0
+    n = len(df)
+    t = time()
+
+    def img(row):
+        global i, d, t, df
+        if((df["img"][i] is None) or (df["img"][i] != df["img"][i])):
+            df["img"][i] = get_image(row)
+            if(i % 100 == 0):
+                save_dataframes(df, path + '.tmp')
+                save_dataframes(df, path)
+        else:
+            d += 1
+            t = time()
+        i += 1
+        dt = time() - t
+        if(i-d > 0):
+            print(f'{((i-d)*1000 // (n-d))/10 }% ({round(dt, 2)}s : {round(((n-d) - (i-d)) * dt / (i-d), 2)}s = {round((n-d) * dt / (i-d), 2)}s)         ', end='\r')
+        pass
+
+    df.apply(img, axis=1)
+
+
+add_image("merged_sales_ratings_img")
