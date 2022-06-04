c9afd35dc68d6338b7141d1227cf045687e8bf72
Rygone <44498456+Rygone@users.noreply.github.com>
2022-06-04 13:54:15 +0200

    comments

--- cercle/index.js
+++ cercle/index.js
@@ -1,6 +1,9 @@
+// Basic functions
 max = (a, b) => a > b ? a : b;
 min = (a, b) => a < b ? a : b;
 abs = (a) => a < 0 ? -a : a;
+
+// Constants
 const rad = 100;
 const color_range = d3.scale.linear()
     .domain([-1, 0, 25, 50, 75, 100])
@@ -15,6 +18,7 @@ const box_height = 270;
 var top_text = null;
 var top_text_root = null;
 
+// Add test to a svg element
 function add_text(svg, dx, dy, text) {
     return svg.append("text")
         .attr("dx", dx)
@@ -27,19 +31,31 @@ function add_text(svg, dx, dy, text) {
         .html(text)
 }
 
+// return new position in the direction of the target
+function smooth_move(curent, target, speed, direction = 1) {
+    return curent - speed * direction * (curent - target);
+}
+
+// draw the bubble of a json
 function draw(json, svg_, value_, score_) {
     let width = window.innerWidth;
     let height = window.innerHeight - 75;
 
+    // clear function to remove sub part
     let clear = function () { }
 
+    // get root svg if none given
     let svg = svg_ == null ? d3.select("svg")
         .style("background-color", 'lightgray')
         : svg_;
+
+    // add basic text
     if (top_text == null) {
         top_text_root = svg;
         top_text = add_text(top_text_root, 20, 30, "")
     }
+
+    // compute sum of value
     let sum = 0;
     let i = 0;
     let n = json.children.length;
@@ -48,6 +64,7 @@ function draw(json, svg_, value_, score_) {
         i++;
     }
 
+    // generate nodes
     json.nodes = d3.range(json.children.length).map(function (i) {
         const radius = Math.sqrt(value_(json.children[i]));
         const src = json.children[i].img.length > 0 ? json.children[i].img : "default.png";
@@ -85,13 +102,64 @@ function draw(json, svg_, value_, score_) {
         };
     });
 
+    // add gravity forces to all nodes
     var force = d3.layout.force()
         .gravity(0.03)
         .nodes(json.nodes)
         .size([width, height]);
-
     force.start();
 
+    let focus = -1;
+    let play = true;
+    function onclick(d, i) {
+        // release all the nodes
+        if (!play) {
+            let i = 0;
+            let n = json.nodes.length;
+            while (i < n) {
+                json.nodes[i].fixed = false;
+                ++i;
+            }
+            play = true;
+        }
+
+        // set or unset the focus
+        if (focus == i) {
+            // reset the focused node
+            d3.select('#g_' + json.nodes[focus].name)
+                .selectAll("circle")
+                .style("fill", function (d, i) { return i ? 'url(#img_' + d.name + ')' : d.color; });
+            clear();
+
+            d3.select('#t_' + json.nodes[focus].name)
+                .html(function (d, i) { return d.text; })
+
+            // remove text
+            let last_html = top_text.html();
+            top_text.remove();
+            top_text = add_text(top_text_root, 20, 30, last_html.substring(0, last_html.length - json.children[focus].Name.length - 1));
+
+            focus = -1;
+        }
+        else {
+            focus = i;
+
+            // edit the focused node
+            d3.select('#g_' + json.nodes[focus].name)
+                .selectAll("circle")
+                .style("fill", function (d, i) { return d.color; });
+
+            d3.select('#t_' + json.nodes[focus].name)
+                .html("");
+
+            // add text
+            let last_html = top_text.html();
+            top_text.remove();
+            top_text = add_text(top_text_root, 20, 30, last_html + "/" + json.children[focus].Name);
+        }
+    }
+
+    // add image pattern
     svg.selectAll("circle")
         .data(json.nodes.slice(0))
         .enter()
@@ -103,52 +171,14 @@ function draw(json, svg_, value_, score_) {
         .append("image")
         .attr("href", function (d, i) { return d.image; });
 
-    function onclick(d, i) {
-        if (!play) {
-            let i = 0;
-            let n = json.nodes.length;
-            while (i < n) {
-                json.nodes[i].fixed = false;
-                ++i;
-            }
-        }
-        play = true;
-        if (focus == i) {
-            d3.select('#g_' + json.nodes[focus].name)
-                .selectAll("circle")
-                .style("fill", function (d, i) { return i ? 'url(#img_' + d.name + ')' : d.color; });
-            clear();
-
-            d3.select('#t_' + json.nodes[focus].name)
-                .html(function (d, i) { return d.text; })
-
-            let last_html = top_text.html();
-            top_text.remove();
-            top_text = add_text(top_text_root, 20, 30, last_html.substring(0, last_html.length - json.children[focus].Name.length - 1));
-
-            focus = -1;
-        }
-        else {
-            focus = i;
-            d3.select('#g_' + json.nodes[focus].name)
-                .selectAll("circle")
-                .style("fill", function (d, i) { return d.color; });
-
-            d3.select('#t_' + json.nodes[focus].name)
-                .html("");
-
-            let last_html = top_text.html();
-            top_text.remove();
-            top_text = add_text(top_text_root, 20, 30, last_html + "/" + json.children[focus].Name);
-        }
-    }
-
+    // create bubbles groups
     const g = svg.selectAll("circle")
         .data(json.nodes.slice(0))
         .enter()
         .append("g")
         .attr("id", function (d, i) { return 'g_' + d.name; });
 
+    // add the circle background color
     g.append("circle")
         .attr("r", rad)
         .attr("cx", rad)
@@ -158,6 +188,7 @@ function draw(json, svg_, value_, score_) {
         .attr("stroke-width", "2")
         .on("click", onclick);
 
+    // add the image
     g.append("circle")
         .attr("r", rad)
         .attr("cx", rad)
@@ -167,36 +198,31 @@ function draw(json, svg_, value_, score_) {
         .attr("stroke-width", "2")
         .on("click", onclick);
 
+    // add the text label
     add_text(g, rad, rad * 2 / 5, function (d, i) { return d.text; })
         .attr("id", function (d, i) { return 't_' + d.name; })
         .attr("dominant-baseline", "middle")
         .attr("text-anchor", "middle")
         .on("click", onclick);
 
-
-    let focus = -1;
-    let play = true;
-
-    function smooth_move(curent, target, speed, direction = 1) {
-        return curent - speed * direction * (curent - target);
-    }
-
+    // return display radius
     function show_radius(radius, width, height) {
         return max(radius * Math.sqrt((width * height) / (sum * 20)), 10);
     }
 
-    let sub_svg = null
+    let sub_svg = null;
 
     force.on("tick", function (e) {
+        // update the width and height of the window
         width = smooth_move(width, window.innerWidth, move_speed);
         height = smooth_move(height, window.innerHeight - 75, move_speed);
-
-        force.size([width, height]);
 
         if (svg_ == null)
             svg.attr("width", width)
                 .attr("height", height);
+        force.size([width, height]);
 
+        // update interval element
         if (sub_svg != null) {
             sub_svg.style("transform", function (d, i) {
                 let x = (width - 200) / box_width
@@ -206,12 +232,14 @@ function draw(json, svg_, value_, score_) {
             })
         }
 
+        // update position and radius of nodes 
         if (play) {
             if (focus >= 0) {
                 let dif = 0;
                 let i = 0;
                 let n = json.nodes.length;
 
+                // update position
                 while (i < n) {
                     root = json.nodes[i];
                     if (focus == i) {
@@ -225,7 +253,10 @@ function draw(json, svg_, value_, score_) {
                     }
                     ++i;
                 }
+
+                // add sub part
                 if (dif < 10) {
+                    // fix all element
                     let i = 0;
                     while (i < n) {
                         json.nodes[i].fixed = true;
@@ -233,7 +264,9 @@ function draw(json, svg_, value_, score_) {
                     }
                     play = false;
 
+                    // create sub part
                     if (json.children[focus].children) {
+                        // add node information
                         clear = draw(
                             json.children[focus],
                             svg.append("g").attr("id", 'gc_' + json.nodes[focus].name),
@@ -241,13 +274,11 @@ function draw(json, svg_, value_, score_) {
                             score_);
                     }
                     else {
+                        // add leaf information
                         let root = json.children[focus]
                         sub_svg = svg
                             .append("g")
-                            .attr("id", 'gc_' + json.nodes[focus].name)
-                            .on("tick", function (e) {
-
-                            });
+                            .attr("id", 'gc_' + json.nodes[focus].name);
 
                         sub_svg.append("rect")
                             .attr("width", box_width)
@@ -272,10 +303,9 @@ function draw(json, svg_, value_, score_) {
                 }
             }
 
-            let q = d3.geom.quadtree(json.nodes);
+            // update radius
             let i = 0;
             let n = json.nodes.length;
-
             while (i < n) {
                 root = json.nodes[i];
                 if (focus == i)
@@ -287,12 +317,15 @@ function draw(json, svg_, value_, score_) {
                 ++i;
             }
 
+            // check collide
+            let q = d3.geom.quadtree(json.nodes);
             i = 0;
             while (i < n) {
                 q.visit(collide(json.nodes[i], width, height));
                 ++i;
             }
 
+            // move all nodes in the window
             if (focus < 0) {
                 i = 0;
                 while (i < n) {
@@ -302,6 +335,7 @@ function draw(json, svg_, value_, score_) {
                 }
             }
 
+            // display updates
             svg.selectAll("image")
                 .attr("x", function (d) { return d.width < d.height ? ((2 * rad) - d.width) / 2 : 0; })
                 .attr("y", function (d) { return d.width > d.height ? ((2 * rad) - d.height) / 2 : 0; });
@@ -316,11 +350,12 @@ function draw(json, svg_, value_, score_) {
         }
     });
 
-
+    // restart animation
     svg.on("mousemove", function () {
         force.resume();
     });
 
+    // check clisions source : https://bl.ocks.org/mbostock/3231298
     function collide(node, width, height) {
         var r = show_radius(node.radius, width, height) * 10,
             nx1 = node.x - r,
@@ -344,13 +379,15 @@ function draw(json, svg_, value_, score_) {
             return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
         };
     }
+
+    // return clear function
     return function () {
         svg.remove();
         force.stop();
     }
 }
 
-
+// get json
 $.getJSON("../data/bubble.json", function (json) {
     draw(json, null, function (child) { return child.Global_Sales }, function (child) { return child.NB ? child.Meta_score / child.NB : child.Meta_score });
 }
